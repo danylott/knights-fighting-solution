65466c70b7710e6104a404d18ae86ff0e72af90b
danylott <d.tuytuyshkin@gmail.com>
2022-01-23 10:59:46 +0200

    Solution for creative task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-# TODO: add initial code
-def hello_world():
-    return "Hello, world!"
+from app.config import KNIGHTS
+from app.players.knight import Knight
+
+
+def main(knights_config: dict):
+    lancelot, arthur, mordred, red_knight = (Knight.from_dict(knight) for knight in knights_config.values())
+    lancelot.battle(mordred)
+    arthur.battle(red_knight)
+    return {
+        lancelot.name: lancelot.hp,
+        arthur.name: arthur.hp,
+        mordred.name: mordred.hp,
+        red_knight.name: red_knight.hp,
+    }
+
+
+if __name__ == '__main__':
+    print(main(KNIGHTS))
